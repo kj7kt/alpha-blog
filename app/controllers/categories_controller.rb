8473256d6c531d7e353071969ccf8329e4ca75fc
kj7kt <yyhuton@gmail.com>
2022-08-18 00:14:34 +0900

    add new template for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,14 +5,26 @@ class CategoriesController < ApplicationController
   end
 
   def new
-
+    @category = Category.new
   end
 
   def create
-
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "New category successfully created"
+      redirect_to @category
+    else
+      render 'new'
+    end
   end
 
   def index
 
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
