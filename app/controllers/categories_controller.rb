b2189b5568a43c2124b242a1e7d9e73c99017923
kj7kt <yyhuton@gmail.com>
2022-08-18 23:58:14 +0900

    add categories feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,8 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
 
   def show
-
+    @category = Category.find(params[:id])
   end
 
   def new
@@ -14,17 +15,24 @@ class CategoriesController < ApplicationController
       flash[:notice] = "New category successfully created"
       redirect_to @category
     else
-      render 'new'
+      render 'new', status: :unprocessable_entity
     end
   end
 
   def index
-
+    @categories = Category.page(params[:page]).per(5)
   end
 
   private
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    unless logged_in? && current_user.admin?
+      flash[:alert] = " Only admins can perform that action"
+      redirect_to categories_path
+    end
   end
 end
